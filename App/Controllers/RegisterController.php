4ba8cf821946e0868a3bd4f255005adbfd4849bd
yuexia <flencle@gmail.com>
2016-11-28 14:07:23 +1300

    password

--- App/Controllers/RegisterController.php
+++ App/Controllers/RegisterController.php
@@ -1,13 +1,49 @@
 <?php
 namespace App\Controllers;
-
 use App\Views\RegisterView;
-
 Class RegisterController
 {
 	public function show(){
 		$view = new RegisterView();
-     	$view->render();
-     	
+      	$view->render();
 	}
+	public function store() {
+		// Prepare a container to hold all the error messages
+		$errors = [];
+		// Validate the form
+		// Each field has been filled out
+		// Email pattern
+		$emailPattern = '/^[a-zA-Z0-9_\-.]{1,100}@[a-zA-Z0-9_\-.]{1,100}\.[a-zA-Z.]{1,100}$/';
+		if( preg_match($emailPattern, $_POST['email']) ) {
+			// Check database
+			die('Email good, check database');
+		} else {
+			// Generate error message
+			$errors['email'] = 'Please enter a valid E-Mail address';
+			
+		}
+		// Passwords match and are at least 8 characters long
+		if( strlen($_POST['password']) == 0 ) {
+			// Password has not been provided
+			$errors['password'] = 'Required';
+		} elseif( strlen($_POST['password']) < 8 ) {
+			$errors['password'] = 'Must be at least 8 characters';
+		} elseif( $_POST['password'] != $_POST['password2'] ) {
+			$errors['password'] = 'Passwords do not match';
+		}
+		// If validation failed
+		if( count($errors) > 0 ) {
+			// Oh dear, errors.
+			$view = new RegisterView($errors);
+      		$view->render();
+      		return;
+		}
+		// If everything is good to go:
+		// Hash the password (don't save it plain text)
+		// Insert new user into database
+		// Log them in automatically (because we're nice)
+		// Redirect to account page
+	}
+	 
+	
 }
--- templates/register.inc.php
+++ templates/register.inc.php
@@ -1,30 +1,43 @@
 
           <h1>Register new account</h1>
-          <form action="./?page=register.store" method="post">
 
-          <div>
-              <label for="email">E-Mail address:</label>
-              <input type="email" name="email" id="email">
-          </div>
+<form action="./?page=register.store" method="post">
+  
+  <div>
+    <label for="email">E-Mail address: </label>
 
-          <div>
-              <label for="password">Password:</label>
-              <input type="password" name="password" id="password">
-          </div>
+    <?php $email = isset($_POST['email']) ? $_POST['email'] : '' ?>
 
-          <div>
-              <label for="password2">Comfirm Password:</label>
-              <input type="password" name="password2" id="password2">
-          </div>
+    <?php
+    // SEE ABOVE
+      // if( isset($_POST['email']) ) {
+      //  $email = $_POST['email'];
+      // } else {
+      //  $email = '';
+      // }
+    ?>
+    
+    <input type="email" value="<?= $email ?>" name="email" id="email">
 
+    <span class="error">
+      <?= isset($this->data['email']) ? $this->data['email'] : '' ?>
+    </span>
+  </div>
 
-          <div>
-              <input type="submit" name="register" value="Register!">
-          </div>
+  <div>
+    <label for="password">Password (8 characters minimum): </label>
+    <input type="password" name="password" id="password">
 
+    <?= isset($this->data['password']) ? $this->data['password'] : '' ?>
+  </div>
 
+  <div>
+    <label for="password2">Confirm Password: </label>
+    <input type="password" name="password2" id="password2">
+  </div>
 
+  <div>
+    <input type="submit" name="register" value="Register!">
+  </div>
 
-            
-          </form>
-        
+</form>
